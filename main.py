e2272edb843e7a66ba9240abfe986d1efec21ed0
Claudio Vieira <claudion@cielo.com.br>
2024-07-25 16:14:03 -0300

    adicionando arquivos

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ import json
 
 if __name__ == "__main__":
     #Ler pdf
-    pdf_file = r'docs\PL_2338.pdf'
+    pdf_file = r"C:\Users\claudion\OneDrive - Cielo\02. Maritalk\docs\Why You (Currently) Do Not Need Deep Learning for Time Series Forecasting _ by Jonte Dancker _ Jun, 2024 _ Towards Data Science.pdf"
     text = lerpdf(pdf_file)
     #Salvar texto em json com utf-8
-    with open('docs/PL_2338.json', 'w', encoding='utf-8') as file:
+    with open('docs/Do Not Need Deep Learning for Time Series Forecasting.json', 'w', encoding='utf-8') as file:
         json.dump(text, file, ensure_ascii=False)
 
     resum = prompt_user(text, r'key.txt', max_lenght=3000)
 
     #Salvar resumo em txt utf-8
-    with open('docs/PL_2338_resumo.txt', 'w', encoding='utf-8') as file:
+    with open('docs/Do Not Need Deep Learning for Time Series Forecasting.txt', 'w', encoding='utf-8') as file:
         file.write(resum)
